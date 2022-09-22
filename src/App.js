f3990ccb1a8ee44440f70ab3d230a0c3b1eae758
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-22 10:23:40 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import About from './Components/About';
 import Experience from './Components/Experience';
-import Footer from './Components/Footer';
-import Skills from './Components/Skills';
-import Projects from './Components/Projects';
+// import Footer from './Components/Footer';
+// import Skills from './Components/Skills';
+// import Projects from './Components/Projects';
 import TopNav from './Components/TopNav';
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
     <div className="app">
       <TopNav />
       <About />
-      <Skills />
+      {/* <Skills />
       <Projects />
       <Experience />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
